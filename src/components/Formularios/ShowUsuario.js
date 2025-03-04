0b0c2266c08ff573aa452b0aadc93e70482b81cb
DanieLSBTH <josuemarque15@gmail.com>
2025-03-04 10:49:31 -0600

    sdf

--- src/components/Formularios/ShowUsuario.js
+++ src/components/Formularios/ShowUsuario.js
@@ -21,6 +21,7 @@ import logo12 from '../Images/Cursos/cisco.jpg';
 import logo13 from '../Images/Cursos/IA_HELSINK.png';
 import logo14 from '../Images/Cursos/AWS_FSE.jpg';
 import logo15 from '../Images/Cursos/AWS_MYSQL.jpg';
+import logo16 from '../Images/Cursos/AWS-Ransomware.jpg';
 
 const ShowUsuario = () => {
   const [modalOpen, setModalOpen] = useState(false); // Estado para controlar el modal
@@ -111,6 +112,16 @@ const ShowUsuario = () => {
                 <p className="text-muted">En proceso</p>
                 <ProgressBar mode="indeterminate" style={{ height: '6px' }}></ProgressBar>
        
+              </Card>
+            </Col>
+            <Col md={4} className="mb-4">
+              <Card className="p-4 shadow-sm">
+                <h4>AWS Security Incident Response - Ransomware Use Case<br /><br /></h4>
+                <p className="text-muted">AWS training certification</p>
+                <p className="text-muted">2025</p>
+                <Button color="primary" size="sm" onClick={() => openModal(logo16)}>
+                  Ver Certificado
+                </Button>
               </Card>
             </Col>
             <Col md={4} className="mb-4">
